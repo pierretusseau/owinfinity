8e5a797a3cd2fa25304fbe2fab5ecd00cdc0a02e
Pierre Tusseau <pierre.tusseau@gmail.com>
2016-11-28 19:03:34 +0100

    Intégration basique frontV2.0 terminée

--- page-templates/frontv2.php
+++ page-templates/frontv2.php
@@ -12,7 +12,7 @@ get_header(); ?>
 <div class="main-wrap">
 	<section id="listing-hero">
 		<div class="wrap--partie-1 row">
-			<section id="left-column" class="column small-6">
+			<section id="listing--left-column" class="column small-6">
 				<h3>Attack</h3>
 				<ul class="home--list-heroes" id="homme--list-attack">
 					<li>Perso</li>
@@ -49,7 +49,7 @@ get_header(); ?>
 					<li>Perso</li>
 				</ul>
 			</section>
-			<section id="right-column" class="column small-6">
+			<section id="listing--right-column" class="column small-6">
 				<!-- REMPLIR ICI AVEC LE NON:HOVER EN ATTENDANT -->
 				<div id="home--video">
 					<h3>Last hero video</h3>
@@ -79,8 +79,79 @@ get_header(); ?>
 			</section>
 		</div>
 	</section>
+	<section id="home--separator">
+		<!-- EMPTY -->
+	</section>
 	<section id="home-second-part">
-		<h2>PARTIE 2</h2>
+		<div class="row" id="home--partie-2-title">
+			<h3 class="column small-9">Blog</h3>
+			<h3 class="column small-3">Twitch</h3>
+		</div>
+		<div class="row" id="home--partie-2-equalizedblog" data-equalizer="liste-article">
+			<section id="home--wrap-listblog" class="column small-9">
+				<ul id="home--list-blog" class="row">
+					<li class="column small-4">
+						<a href="#">
+							<h4>Titre de l'article</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+						</a>
+					</li>
+					<li class="column small-4">
+						<a href="#">
+							<h4>Titre de l'article</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+						</a>
+					</li>
+					<li class="column small-4">
+						<a href="#">
+							<h4>Titre de l'article</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+						</a>
+					</li>
+					<li class="column small-4">
+						<a href="#">
+							<h4>Titre de l'article</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+						</a>
+					</li>
+					<li class="column small-4">
+						<a href="#">
+							<h4>Titre de l'article</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+						</a>
+					</li>
+					<li class="column small-4">
+						<a href="#">
+							<h4>Titre de l'article</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+						</a>
+					</li>
+				</ul>
+			</section>
+
+			<section id="home--twitch-block" class="column small-3" data-equalizer-watch="liste-article">
+				<div id="twitch-wrap" data-equalizer-watch="liste-article">
+					<div id="twitch-stream">
+						<!-- EMPTY -->
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus laborum nobis dolorum velit expedita accusantium quam quod, neque at veritatis.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, sequi, id! Debitis voluptatibus ad sint, veritatis sunt culpa est quam.</p>
+				</div>
+			</section>
+		</div>
+		<div class="row" id="home--partie-2-buttons">
+			<section id="community--left-column" class="column small-9">
+				<a href="#" class="expanded button">
+					Read more
+				</a>
+			</section>
+			<section id="community--right-column" class="column small-3">
+				<div class="expanded button-group align-justify">
+					<a href="#" class="button">Twitter</a>
+					<a href="#" class="button">Tipeee</a>
+				</div>
+			</section>
+		</div>
 	</section>
 </div>
 <?php get_footer();
