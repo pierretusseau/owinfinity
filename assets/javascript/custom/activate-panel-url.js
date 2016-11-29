50dfceb67fbf4024436d30bcfd6ea4e062d6baff
Kevin Bacas <bacas.kevin@hotmail.fr>
2016-11-30 00:42:21 +0100

    Management for the skill parameter in every hero page

--- assets/javascript/custom/activate-panel-url.js
+++ assets/javascript/custom/activate-panel-url.js
@@ -1,15 +1,60 @@
-function clickOnPrimary() {
-  setTimeout(function() {
-    $('#lclick-button').click();
-  }, 1000);
+// Polyfill pour String.includes
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/includes
+if ( !String.prototype.includes ) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    } 
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search,start) !== -1;
+    }
+  };
 }
 
-var manageClickOnPrimary = function () {
-  if(window.isBindingClickEvents || window.isBindingClickEvents === undefined) {
-    setTimeout(manageClickOnPrimary, 200);
-  } else {
-    clickOnPrimary();
-  }
+var parseQueryString = function( queryString ) {
+    var params = {}, queries, temp, i, l;
+    // Split into key/value pairs
+    queries = queryString.split("&amp;");
+    // Convert the array of strings into an object
+    for ( i = 0, l = queries.length; i < l; i++ ) {
+        temp = queries[i].split('=');
+        params[temp[0]] = temp[1];
+    }
+    return params;
 };
 
-manageClickOnPrimary();
+function clickOnElementAfterTwoSecond(elem) {
+  return setTimeout(function() {
+    elem.click();
+  }, 2000);
+} 
+
+function clickOnPrimary() {
+  return clickOnElementAfterTwoSecond($('#lclick-button'));
+}
+
+var url = window.location.href;
+var queryString = url.substring(url.indexOf('?') + 1);
+var parsedQueryString = parseQueryString(queryString);
+
+if (url.includes('/hero/')) {
+  if (parsedQueryString.skill) {
+    var button = document.getElementById(parsedQueryString.skill + '-button');
+    if (button) {
+      if(!$(button).hasClass('disabled')) {
+        clickOnElementAfterTwoSecond($(button));
+      } else {
+        manageClickOnPrimary();
+      }
+    } else {
+      manageClickOnPrimary();
+    }
+  } else {
+    manageClickOnPrimary();
+  }
+
+}
