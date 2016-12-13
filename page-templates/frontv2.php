0095386c1201763778784adcb37c483eabcdbc61
Pierre Tusseau <pierre.tusseau@gmail.com>
2016-12-13 18:30:01 +0100

    FontAwesome some social icons

--- page-templates/frontv2.php
+++ page-templates/frontv2.php
@@ -78,8 +78,8 @@ get_header(); ?>
 			</section>
 			<section id="community--right-column" class="column small-3">
 				<div class="expanded button-group align-justify">
-					<a href="<?php the_field('twitter_link') ?>" class="button" id="home--twitter">Twitter</a>
-					<a href="<?php the_field('tipeee_link') ?>" class="button" id="home--tipeee">Github</a>
+					<a href="<?php the_field('twitter_link') ?>" class="button" id="home--twitter"><i class="fa fa-twitter"></i></a>
+					<a href="<?php the_field('tipeee_link') ?>" class="button" id="home--tipeee"><i class="fa fa-github"></i></a>
 				</div>
 			</section>
 		</div>
